5afdb6cf1b558f32ccb82ec5f21f674deedad512
Diogo Pinela <diogoid7400@gmail.com>
2023-09-03 00:45:50 +0100

    Read the entire chip sprite sheet

--- InventoryWebTracker/InventoryWebTrackerPlugin.cs
+++ InventoryWebTracker/InventoryWebTrackerPlugin.cs
@@ -1,5 +1,8 @@
 global using System;
+using SyncCollections = System.Collections.Concurrent;
+using Tasks = System.Threading.Tasks;
 using Bep = BepInEx;
+using UE = UnityEngine;
 using static EmbedIO.WebApi.WebApiModuleExtensions;
 using static EmbedIO.WebServerOptionsExtensions;
 using static EmbedIO.WebModuleContainerExtensions;
@@ -9,8 +12,12 @@ namespace Haiku.InventoryWebTracker
     [Bep.BepInDependency("haiku.mapi", "1.0")]
     public class InventoryWebTrackerPlugin : Bep.BaseUnityPlugin
     {
+        private static InventoryWebTrackerPlugin? plugin;
+        internal static InventoryWebTrackerPlugin Instance => plugin!;
+
         public void Start()
         {
+            plugin = this;
             modSettings = new(Config);
         }
 
@@ -29,11 +36,54 @@ namespace Haiku.InventoryWebTracker
                 uiServer.Dispose();
                 uiServer = null;
             }
+
+            if (iconFetchQueue.TryDequeue(out var task))
+            {
+                task.RunSynchronously();
+            }
         }
 
         private const string url = "http://localhost:8086";
 
         private Settings? modSettings;
         private EmbedIO.WebServer? uiServer;
+
+        private SyncCollections.ConcurrentQueue<Tasks.Task<byte[]>> iconFetchQueue = new();
+
+        internal Tasks.Task<byte[]> ToPNGAsync(Func<UE.Sprite> getter)
+        {
+            var task = new Tasks.Task<byte[]>(() => ToPNG(getter()));
+            iconFetchQueue.Enqueue(task);
+            return task;
+        }
+
+        private byte[] ToPNG(UE.Sprite sprite)
+        {
+            var srcRect = sprite.textureRect;
+            var act = UE.RenderTexture.active;
+            //var tmp = UE.RenderTexture.GetTemporary((int)srcRect.width, (int)srcRect.height);
+            var tmp = UE.RenderTexture.GetTemporary(sprite.texture.width, sprite.texture.height, 0, UE.RenderTextureFormat.ARGB32);
+            try
+            {
+                UE.RenderTexture.active = tmp;
+                UE.Graphics.Blit(sprite.texture, tmp);
+                //UE.Graphics.CopyTexture(sprite.texture, 0, 0, 0, 0, tmp.width, tmp.height, tmp, 0, 0, 0, 0);
+                var buf = new UE.Texture2D(tmp.width, tmp.height, UE.TextureFormat.ARGB32, false);
+                buf.ReadPixels(new(0, 0, tmp.width, tmp.height), 0, 0, false);
+                var png = UE.ImageConversion.EncodeToPNG(buf);
+                UE.Object.Destroy(buf);
+                return png;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+                throw;
+            }
+            finally
+            {
+                UE.RenderTexture.active = act;
+                UE.RenderTexture.ReleaseTemporary(tmp);
+            }
+        }
     }
 }
--- InventoryWebTracker/WebUI.cs
+++ InventoryWebTracker/WebUI.cs
@@ -1,6 +1,8 @@
 using Routing = EmbedIO.Routing;
 using WAPI = EmbedIO.WebApi;
 using static EmbedIO.HttpContextExtensions;
+using UE = UnityEngine;
+using Collections = System.Collections.Generic;
 using Tasks = System.Threading.Tasks;
 using Text = System.Text;
 
@@ -11,16 +13,33 @@ namespace Haiku.InventoryWebTracker
         [Routing.Route(EmbedIO.HttpVerbs.Get, "/")]
         public async Tasks.Task GetUI()
         {
+            await HttpContext.SendStringAsync(UI, "text/html; charset=utf-8", Text.Encoding.UTF8);
+        }
+
+        [Routing.Route(EmbedIO.HttpVerbs.Get, "/icons/{iconName}")]
+        public async Tasks.Task GetIcon(string iconName)
+        {
+            if (!gameIcons.TryGetValue(iconName, out var icon))
+            {
+                throw EmbedIO.HttpException.NotFound();
+            }
+            HttpContext.Response.ContentType = "image/png";
+            var png = await InventoryWebTrackerPlugin.Instance.ToPNGAsync(icon);
             using var stream = HttpContext.OpenResponseStream();
-            var b = Text.Encoding.UTF8.GetBytes(UI);
-            await stream.WriteAsync(b, 0, b.Length);
+            await stream.WriteAsync(png, 0, png.Length);
         }
 
         private const string UI = @"<!doctype html>
         <html>
         <body>
             <p>Hello!</p>
+            <img src='/icons/chip.png'>
         </body>
         </html>";
+
+        private static readonly Collections.Dictionary<string, Func<UE.Sprite>> gameIcons = new()
+        {
+            {"chip.png", () => GameManager.instance.chip[3].image}
+        };
     }
 }
